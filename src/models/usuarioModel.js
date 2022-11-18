6aab7343ec59ad4abfc68bd226c0996af5de739d
gabs-mvb <gabriel.boas@sptech.school>
2022-11-18 10:54:03 -0300

    Instrução alterada para dar select na tabela usuario

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -12,7 +12,7 @@ function listar() {
 function entrar(user, senha) {
     console.log("ACESSEI O Perfil MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", user, senha)
     var instrucao = `
-        SELECT * FROM Perfil WHERE username = '${user}' AND senha = '${senha}';
+        SELECT * FROM usuario WHERE username = '${user}' AND senha = '${senha}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
